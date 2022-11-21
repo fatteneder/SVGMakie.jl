1df6358b41f62d25d9dccfd75eab880688e42cd6
Florian Atteneder <florian.atteneder@gmail.com>
2022-11-21 15:43:04 +0100

    implement svg_color

--- src/utils.jl
+++ src/utils.jl
@@ -1,1 +1,7 @@
+
+# recognized color keywords: https://www.w3.org/TR/SVG11/types.html#ColorKeywords
 svg_color(color) = string(color)
+svg_color(color::Colorant) = "\"rgb($(color.r),$(color.g),$(color.b))\""
+
+svg_color_alpha(color) = ""
+svg_color_alpha(color::TransparentColor) = "\"$(color.alpha)\""
